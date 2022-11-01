fafd25665cc101446cadd465de44260b21230d8a
Yassinmoh <yassin_mohamed310@outlook.com>
2022-11-01 18:20:28 +0200

    create views

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xA3A3A3)
 
 
-//controls
+// controls
 // const controls =new FirstPersonControls(camera ,renderer.domElement)
 // controls.movementSpeed =8
 // controls.lookSpeed =0.08
@@ -36,22 +36,55 @@ loadingManager.onLoad = function () {
 }
 
 //image
+let position = 0;
 const glftLoader = new GLTFLoader(loadingManager)
 glftLoader.load("./assets/the_king_s_hall/scene.gltf", function (gltf) {
     const modal = gltf.scene;
     scene.add(modal)
 
     window.addEventListener("mouseup", function () { //mousewheel
-        moveCamera(-1.8, 1.6, 5, 3)
-        rotateCamera(0, 0.1, 0, 3.2)
+        console.log(camera.position);
+        switch (position) {
+            case 0:
+                moveCamera(-1.8, 1.6, 5)
+                rotateCamera(0, 0.1, 0)
+                position = 1;
+                break;
+            case 1:
+                moveCamera(2.8, 0, 3.6)
+                rotateCamera(0, -1.6, 0)
+                position = 2;
+                break;
+            case 2:
+                moveCamera(2.5, -0.9, 12.2)
+                rotateCamera(0.9, 0.6, -0.6)
+                position = 3;
+                break;
+            case 3:
+                moveCamera(-2.5, 0.7, 4)
+                rotateCamera(0.6, 1.9, -0.6)
+                position = 4;
+                break;
+            case 4:
+                moveCamera(-1.7, 0, 8.7)
+                rotateCamera(0, 4.7, 0)
+                position = 5;
+                break;
+            case 5:
+                moveCamera(0.5, 0.8, 10)
+                rotateCamera(0.3, 1.65, -0.3)
+                position = 0;
+
+        }
+
 
     })
 
-    const moveCamera = (x, y, z, duration) => {
-        gsap.to(camera.position, { x, y, z, duration })
+    const moveCamera = (x, y, z) => {
+        gsap.to(camera.position, { x, y, z, duration: 3 })
     }
-    const rotateCamera = (x, y, z, duration) => {
-        gsap.to(camera.rotation, { x, y, z, duration })
+    const rotateCamera = (x, y, z) => {
+        gsap.to(camera.rotation, { x, y, z, duration: 3.2 })
     }
 })
 
